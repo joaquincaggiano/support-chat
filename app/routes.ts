13915151c80adfe3f3b2f5a53b4904dac142df5c
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-07-22 18:27:18 +0200

    feat(): auth layout

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,20 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+  prefix,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // Home
+  index("routes/home.tsx"),
+
+  // Auth
+  ...prefix("/auth", [
+    layout("layouts/auth-layout.tsx", [
+      route("login", "routes/auth/login-page.tsx"),
+      route("register", "routes/auth/register-page.tsx"),
+    ]),
+  ]),
+] satisfies RouteConfig;
