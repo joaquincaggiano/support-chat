b5babfa272764cb7f8199d12a8c8d3a024077dac
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-07-24 20:13:37 +0200

    feat(): actions and client actions

--- app/routes/auth/testing-page.tsx
+++ app/routes/auth/testing-page.tsx
@@ -1,5 +1,18 @@
-import { NavLink } from "react-router";
+import { Form, NavLink } from "react-router";
 import type { Route } from "./+types/testing-page";
+
+export async function action({ request }: Route.ActionArgs) {
+  const data = await request.formData();
+  console.log({ data });
+
+  return { message: "Hello, world! from action" };
+}
+
+export async function clientAction({ serverAction }: Route.ClientActionArgs) {
+  const serverData = await serverAction();
+
+  return { message: "Hello, world! from client action", serverData };
+}
 
 export async function loader() {
   return { message: "Hello, world! from loader" };
@@ -35,6 +48,24 @@ export default function MyRouteComponent({
       >
         Testing Args Page
       </NavLink>
+
+      <Form className="flex flex-col gap-2 my-10" method="post">
+        <input
+          className="border border-gray-300 rounded-md p-2"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          className="border border-gray-300 rounded-md p-2"
+          type="text"
+          name="age"
+          placeholder="Age"
+        />
+        <button className="bg-blue-500 text-white rounded-md p-2" type="submit">
+          Submit
+        </button>
+      </Form>
     </div>
   );
 }
