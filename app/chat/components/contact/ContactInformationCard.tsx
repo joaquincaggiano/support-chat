be89586bfb8e3f8a62cbe04773f6c128d39fa404
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-07-27 21:54:51 +0200

    feat(): loader information client

--- app/chat/components/contact/ContactInformationCard.tsx
+++ app/chat/components/contact/ContactInformationCard.tsx
@@ -1,13 +1,33 @@
+import { useLoaderData, useNavigation, useParams } from "react-router";
 import ContactInformation from "./ContactInformation";
 import ContactInformationSkeleton from "./ContactInformationSkeleton";
 import NoContactSelected from "./NoContactSelected";
+import type { Client } from "~/chat/interfaces/chat.interface";
 
 const ContactInformationCard = () => {
-  return <NoContactSelected />;
+  const { id } = useParams();
+  // Debemos saber cual es el padre de este componente, para saber que data nos traerá useLoaderData
+  const { clients = [] } = useLoaderData();
 
-  return <ContactInformationSkeleton />;
+  const { state } = useNavigation();
 
-  return <ContactInformation />;
+  const isPending = state === "loading";
+
+  if (!id) {
+    return <NoContactSelected />;
+  }
+
+  const client = clients.find((client: Client) => client.id === id);
+
+  if (!client) {
+    return <NoContactSelected />;
+  }
+
+  if (isPending) {
+    return <ContactInformationSkeleton />;
+  }
+
+  return <ContactInformation client={client} />;
 };
 
 export default ContactInformationCard;
